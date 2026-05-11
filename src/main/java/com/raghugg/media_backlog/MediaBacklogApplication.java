283ff4cdb3e077a251c732d02a40c04cc60c6d22
raghugg <guggilam.raghu@gmail.com>
2026-05-10 22:56:07 -0400

    Initialize project with Maven setup and Spring Boot application

---
+++ src/main/java/com/raghugg/media_backlog/MediaBacklogApplication.java
@@ -0,0 +1,13 @@
+package com.raghugg.media_backlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaBacklogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaBacklogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raghugg/media_backlog/MediaBacklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghugg.media_backlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaBacklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
