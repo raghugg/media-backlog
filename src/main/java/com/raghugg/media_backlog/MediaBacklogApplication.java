eec984b80f582a51ca0155d3670bebea8bb61485
raghugg <guggilam.raghu@gmail.com>
2026-05-11 02:23:23 -0400

    Add MediaRepository, wire up JPA, and configure PostgreSQL connection

--- src/main/java/com/raghugg/media_backlog/MediaBacklogApplication.java
+++ src/main/java/com/raghugg/media_backlog/MediaBacklogApplication.java
@@ -1,13 +1,32 @@
 package com.raghugg.media_backlog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MediaBacklogApplication {
+public class MediaBacklogApplication implements CommandLineRunner {
+	@Autowired
+	private MediaRepository mediaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MediaBacklogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Add
+		Media m = new Media(null, "Inception", MediaType.MOVIE);
+		mediaRepository.save(m);
+
+		// Display
+		System.out.println(mediaRepository.findAll());
+
+		// Remove
+		mediaRepository.deleteById(m.getId());
+
+		// Display
+		System.out.println(mediaRepository.findAll());
+	}
 }
---
+++ src/main/java/com/raghugg/media_backlog/MediaRepository.java
@@ -0,0 +1,5 @@
+package com.raghugg.media_backlog;
+
+public interface MediaRepository extends org.springframework.data.jpa.repository.JpaRepository<Media, Long> {
+    
+}
