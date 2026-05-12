4226e9a8c0b24e7f060c57bf890c5eb31190c254
raghugg <guggilam.raghu@gmail.com>
2026-05-11 21:12:02 -0400

    Add MediaController to handle media CRUD operations

---
+++ src/main/java/com/raghugg/media_backlog/MediaController.java
@@ -0,0 +1,33 @@
+package com.raghugg.media_backlog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class MediaController {
+    @Autowired
+	private MediaRepository mediaRepository;
+
+    @GetMapping("/media")
+    public List<Media> getMedia() {
+        return mediaRepository.findAll();
+    }
+
+    @PostMapping("/media")
+    public Media postMedia(@RequestBody Media media) {
+        return mediaRepository.save(media);
+    }
+
+    @DeleteMapping("/media/{id}")
+    public ResponseEntity<Void> deleteMedia(@PathVariable Long id) {
+        mediaRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
